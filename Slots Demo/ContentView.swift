2883f8ea09d36dd3191751c2572835929a5e0c0c
AnnaPerehrest <nyayaka23@gmail.com>
2020-08-05 11:09:52 +0300

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Macbook Pro on 05.08.2020.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
